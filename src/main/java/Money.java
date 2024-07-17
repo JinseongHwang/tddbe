ac56582328a6f626bee8be2f0218709243159798
JinseongHwang <jinseong.dev@gmail.com>
2024-07-17 23:50:31 +0900

    현재까지 코드를 살펴보니 Dollar와 Franc이 굳이 필요 없을 것 같아서 제거하고 싶다는 생각이 들었다. 그래서 우선 Dollar와 Franc을 직접적으로 의존하고 있는 부분을 변경해주기로 했다. 테스트 코드에서 Dollor를 팩토리 메서드로 처리하도록 변경했다.
    ---
    하지만 컴파일에 실패한다 ㅠㅠ Money에 times가 없기 때문이다.

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,6 +1,10 @@
 public class Money {
     protected int amount;
 
+    public static Dollar dollar(int amount) {
+        return new Dollar(amount);
+    }
+
     @Override
     public boolean equals(Object obj) {
         Money money = (Money) obj;
