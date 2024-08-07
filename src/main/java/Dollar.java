77498b5709585b697671458c5ea7b02b14abf97e
JinseongHwang <jinseong.dev@gmail.com>
2024-08-08 02:58:59 +0900

    Money로 times 구현을 옮기고 Dollar와 Franc에는 times를 제거해봤다. 여전히 잘 동작한다!

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -3,9 +3,4 @@ public class Dollar extends Money {
     public Dollar(int amount, String currency) {
         super(amount, currency);
     }
-
-    @Override
-    public Money times(int multiplier) {
-        return new Money(amount * multiplier, currency);
-    }
 }
--- src/main/java/Franc.java
+++ src/main/java/Franc.java
@@ -3,9 +3,4 @@ public class Franc extends Money {
     public Franc(int amount, String currency) {
         super(amount, currency);
     }
-
-    @Override
-    public Money times(int multiplier) {
-        return new Money(amount * multiplier, currency);
-    }
 }
